61eea38b8b0ee285323f8ed1b1ac11c64d7087d5
huyvod <duchuya7@gmail.com>
2020-05-05 15:13:23 +0700

    Init

---
+++ DemoCI/DemoCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoCI
+//
+//  Created by MBA0288F on 5/5/20.
+//  Copyright © 2020 MBA0288F. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
